fe00808b0cf756286198c3d186d581f628c918b9
TJ Hillard <tjhillard@gmail.com>
2016-06-28 13:27:30 -0500

    setup for browserstack integration and multi-enviroment testing

--- tests/home.js
+++ tests/home.js
@@ -1,13 +1,17 @@
 module.exports = {
   before: function(browser) {
-    browser.resizeWindow(1920, 1080);
+
   },
   'Home Page' : function (client) {
+    // Test
     client
       .url('http://www.orainteractive.com')
       .waitForElementVisible('body', 1000)
       .waitForElementVisible('.local-scroll a.jumpr', 3000)
-      .click('.local-scroll a.rounder')
+      .pause(1000)
+      .useXpath()
+      .click('//*[@id="vid"]/div[2]/div/div/div/a')
+      .useCss()
       .pause(2000)
       .waitForElementVisible('#welcome .container .row .twelve p.subtitle', 5000)
       .assert.containsText('#welcome .container .row .twelve p.subtitle', 'Hello there, we are')
