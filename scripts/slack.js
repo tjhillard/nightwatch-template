c95ed3f07ebd87c4608b6daa8084e6cf77905196
TJ <tjhillard@gmail.com>
2016-07-21 11:27:10 -0500

    Update slack.js

--- scripts/slack.js
+++ scripts/slack.js
@@ -2,14 +2,14 @@ slack = function(test) {
   require('dotenv').config();
   var Slack = require('slack-node');
 
-  webhookUri = "https://hooks.slack.com/services/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";
+  webhookUri = "{{your-slack-web-hook-uri}}";
   slack = new Slack();
   slack.setWebhook(webhookUri);
 
   console.log(test);
 
   slack.webhook({
-    channel: "@tjhillard",
+    channel: "#general",
     username: "NightBot",
     text: "Uh oh! The " + test.name + " Test failed! { Failed: " + test.results.failed + " Passed: " + test.results.passed + " }"
   }, function(err, response) {
